332d1428c7f49b0093cb5bbc26842daa78ab02bf
BJJIN <bjjins@163.com>
2023-05-07 17:34:11 +0800

    完成时钟

--- packages/clock/index.js
+++ packages/clock/index.js
@@ -1,42 +1,75 @@
 import digit from "./digit.js";
+
+class Ball {
+  g = 1.5 + Math.random();
+  x = 0;
+  y = 0;
+  vx = Math.pow(-1, Math.ceil(Math.random() * 1000)) * 4;
+  vy = Math.pow(-1, Math.ceil(Math.random() * 1000)) * 5;
+  radius = 15;
+  color = "";
+  static colors = ["red", "green", "yellow", "pink", "violet"];
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    const n = ~~(Math.random() * Ball.colors.length);
+    this.color = Ball.colors[n];
+  }
+  update(vy) {
+    this.x += this.vx;
+    this.vy = vy === undefined ? this.vy : vy;
+    this.y += this.vy;
+    this.vy += this.g;
+  }
+}
 class Clock {
   ele;
   /**@type {CanvasRenderingContext2D | null} */
   ctx;
 
-  WINDOW_HEIGHT = 1034;
+  WINDOW_HEIGHT = 400;
   WINDOW_WIDTH = 1000;
   BAIL_RADIUS = 8; //小球半径,则小球所在格子长8*2+1+1，1是球与格子的空隙
   BOX_WIDTH = 18;
   PADDING_TOP = 60; //画框顶部留白
   PADDING_LEFT = 30; //画框左边留白
 
-  hour = 12;
-  minutes = 34;
-  seconds = 56;
-  preSeconds = 0;
+  time = "";
+  preTime = "";
 
   timer;
+  first = true;
+
+  /**@type {Ball[]} */
+  balls = [];
 
   constructor() {
     this.getCanvasEle();
     this.initCanvas();
-    this.timer = requestAnimationFrame(this.rafRender);
+    this.timer = setInterval(() => {
+      this.rafRender();
+    }, 50);
   }
 
   rafRender = () => {
+    this.ctx.clearRect(0, 0, this.WINDOW_WIDTH, this.WINDOW_HEIGHT);
     this.getNow();
-    if (this.seconds !== this.preSeconds) {
-      this.render();
+    this.render();
+    if (this.balls.length) {
+      this.updateBall();
     }
-    this.timer = requestAnimationFrame(this.rafRender);
   };
 
   getNow() {
     const now = new Date();
-    this.hour = now.getHours();
-    this.minutes = now.getMinutes();
-    this.seconds = now.getSeconds();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    this.time = "" + hours + minutes + seconds;
   }
 
   getCanvasEle() {
@@ -50,6 +83,57 @@ class Clock {
   initCanvas() {
     this.ele.height = this.WINDOW_HEIGHT;
     this.ele.width = this.WINDOW_WIDTH;
+  }
+
+  /* --------------------------------- 动画小球相关 --------------------------------- */
+  addBall(x, y, num) {
+    digit[num].forEach((numList, i) => {
+      numList.forEach((_num, j) => {
+        if (_num === 1) {
+          const centerX = x + j * this.BOX_WIDTH - this.BOX_WIDTH / 2;
+          const centerY = y + i * this.BOX_WIDTH - this.BOX_WIDTH / 2;
+          const ball = new Ball(centerX, centerY, this.BAIL_RADIUS);
+          this.balls.push(ball);
+        }
+      });
+    });
+  }
+  clearBalls() {
+    if (this.balls.length) {
+      this.balls = [];
+    }
+  }
+  updateBall() {
+    this.balls.forEach((ball) => {
+      this.ctx.beginPath();
+      this.ctx.fillStyle = ball.color;
+      let vy = ball.vy;
+      if (vy > -2 && vy <= 0) {
+        vy = 0;
+      }
+      if (ball.y + ball.radius >= this.WINDOW_HEIGHT) {
+        vy = -parseInt(vy) * 0.2;
+        ball.y = this.WINDOW_HEIGHT - ball.radius;
+      }
+      this.ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+      this.ctx.fill();
+      ball.update(vy);
+    });
+
+    let cnt = 0;
+    for (let i = 0; i < this.balls.length; i++) {
+      const ball = this.balls[i];
+      if (
+        ball.x + ball.radius > 0 &&
+        ball.x - ball.radius < this.WINDOW_WIDTH
+      ) {
+        //球还在画布中
+        this.balls[cnt++] = this.balls[i];
+      }
+    }
+
+    this.balls.splice(++cnt);
+    console.log(this.balls.length);
   }
 
   /**
@@ -72,33 +156,35 @@ class Clock {
       });
     });
   }
-  stringNow() {
-    const hours = this.hour < 10 ? "0" + this.hour : this.hour;
-    const minutes = this.minutes < 10 ? "0" + this.minutes : this.minutes;
-    const seconds = this.seconds < 10 ? "0" + this.seconds : this.seconds;
-    return "" + hours + minutes + seconds;
-  }
   render() {
-    this.ctx.clearRect(0, 0, this.WINDOW_WIDTH, this.WINDOW_HEIGHT);
     //先从0，0开始渲染hour的十位
-    const time = this.stringNow();
     let positionX = this.PADDING_LEFT;
-    time.split("").forEach((char, i) => {
+    this.time.split("").forEach((char, i) => {
       if (i !== 0) {
         positionX += this.BOX_WIDTH * 8;
       }
-      const num = parseInt(char);
+
+      //绘制冒号
       if ((i & 1) === 0 && i !== 0) {
-        //冒号
         this.renderDigit(positionX, this.PADDING_TOP, digit.length - 1);
         positionX += this.BOX_WIDTH * 4;
       }
+
+      const num = parseInt(char);
+      //动画小球
+      if (!this.first) {
+        if (this.preTime[i] !== char) {
+          this.addBall(positionX, this.PADDING_TOP, num);
+        }
+      }
+      //绘制时间数字
       this.renderDigit(positionX, this.PADDING_TOP, num);
     });
-    this.preSeconds = this.seconds;
+    this.preTime = this.time;
+    this.first = false;
   }
   stop() {
-    cancelAnimationFrame(this.timer);
+    clearInterval(this.timer);
   }
 }
 
