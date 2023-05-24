f4764ac1334b94ee84f60e6976ef1dd346818e0c
BJJIN <bjjins@163.com>
2023-05-24 12:04:28 +0800

    二维矩阵变换

--- packages/webgl/example/utils.js
+++ packages/webgl/example/utils.js
@@ -1,166 +1,136 @@
 function resizeCanvasToDisplaySize(canvas, multiplier) {
-    multiplier = multiplier || 1;
-    const width = (canvas.clientWidth * multiplier) | 0;
-    const height = (canvas.clientHeight * multiplier) | 0;
-    if (canvas.width !== width || canvas.height !== height) {
-        canvas.width = width;
-        canvas.height = height;
-        return true;
-    }
-    return false;
+  multiplier = multiplier || 1;
+  const width = (canvas.clientWidth * multiplier) | 0;
+  const height = (canvas.clientHeight * multiplier) | 0;
+  if (canvas.width !== width || canvas.height !== height) {
+    canvas.width = width;
+    canvas.height = height;
+    return true;
+  }
+  return false;
 }
 
 function createShader(gl, type, source) {
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-    if (success) {
-        return shader;
-    }
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
 
-    console.error(`创建 shader 失败：`, gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
+  console.error(`创建 shader 失败：`, gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
 }
 
 function createProgram(gl, vertexShader, fragmentShader) {
-    const program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-        return program;
-    }
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
 
-    console.error("创建 program 失败：", gl.getProgramInfoLog(program)
-    )
-    ;
-    gl.deleteProgram(program);
+  console.error("创建 program 失败：", gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
 }
 
 function initProgram(canvasId, vertexShaderSource, fragmentShaderSour) {
-    /**@type {HTMLCanvasElement} */
-    const dom = document.getElementById(canvasId);
-    const gl = dom.getContext("webgl");
-    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-    const fragmentShader = createShader(
-        gl,
-        gl.FRAGMENT_SHADER,
-        fragmentShaderSour
-    );
-    const program = createProgram(gl, vertexShader, fragmentShader);
-    gl.useProgram(program);
-    resizeCanvasToDisplaySize(dom);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    clear(gl);
-    return {
-        program,
-        gl,
-    };
+  /**@type {HTMLCanvasElement} */
+  const dom = document.getElementById(canvasId);
+  const gl = dom.getContext("webgl");
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSour);
+  const program = createProgram(gl, vertexShader, fragmentShader);
+  gl.useProgram(program);
+  resizeCanvasToDisplaySize(dom);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  clear(gl);
+  return {
+    program,
+    gl,
+  };
 }
 
 function clear(gl) {
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-
 function setRectangle(gl, x, y, w, h) {
-    const x1 = x;
-    const x2 = x + w;
-    const y1 = y;
-    const y2 = y + h;
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        x1, y1,
-        x2, y1,
-        x1, y2,
-        x1, y2,
-        x2, y1,
-        x2, y2,
-    ]), gl.STATIC_DRAW)
+  const x1 = x;
+  const x2 = x + w;
+  const y1 = y;
+  const y2 = y + h;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]), gl.STATIC_DRAW);
 }
 
 /**
  * @param { WebGLRenderingContext } gl
  */
 function createAndSetupTexture(gl) {
-    const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    // 设置材质，这样我们可以对任意大小的图像进行像素操作
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    return texture;
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  // 设置材质，这样我们可以对任意大小的图像进行像素操作
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  return texture;
 }
 
 const m3 = {
-    translation: (tx, ty) => {
-        return [
-            1, 0, 0,
-            0, 1, 0,
-            tx, ty, 1,
-        ]
-    },
-    rotation: (angleInRadians) => {
-        const cos = +Math.cos(angleInRadians).toFixed(15);
-        const sin = +Math.sin(angleInRadians).toFixed(15);
-        return [
-            cos, sin, 0,
-            -sin, cos, 0,
-            0, 0, 1
-        ]
-    },
-    scale: (sx, sy) => {
-        return [
-            sx, 0, 0,
-            0, sy, 0,
-            0, 0, 1
-        ]
-    },
-    multiply: (ma, mb) => {
-        const ma00 = ma[0 * 3 + 0];
-        const ma01 = ma[0 * 3 + 1];
-        const ma02 = ma[0 * 3 + 2];
+  translation: function (tx, ty) {
+    return [1, 0, 0, 0, 1, 0, tx, ty, 1];
+  },
 
-        const ma10 = ma[1 * 3 + 0];
-        const ma11 = ma[1 * 3 + 1];
-        const ma12 = ma[1 * 3 + 2];
+  rotation: function (angleInRadians) {
+    const c = Math.cos(angleInRadians);
+    const s = Math.sin(angleInRadians);
+    return [
+      c, s, 0, 
+      -s, c, 0, 
+      0, 0, 1];
+  },
 
-        const ma20 = ma[2 * 3 + 0];
-        const ma21 = ma[2 * 3 + 1];
-        const ma22 = ma[2 * 3 + 2];
+  scaling: function (sx, sy) {
+    return [sx, 0, 0, 0, sy, 0, 0, 0, 1];
+  },
 
-        const mb00 = mb[0 * 3 + 0];
-        const mb01 = mb[0 * 3 + 1];
-        const mb02 = mb[0 * 3 + 2];
-
-        const mb10 = mb[1 * 3 + 0];
-        const mb11 = mb[1 * 3 + 1];
-        const mb12 = mb[1 * 3 + 2];
-
-        const mb20 = mb[2 * 3 + 0];
-        const mb21 = mb[2 * 3 + 1];
-        const mb22 = mb[2 * 3 + 2];
-
-
-        return [
-            ma00 * mb00 + ma01 * mb10 + ma02 * mb20,
-            ma00 * mb01 + ma01 * mb11 + ma02 * mb21,
-            ma00 * mb02 + ma01 * mb12 + ma02 * mb22,
-
-            ma10 * mb00 + ma11 * mb10 + ma12 * mb20,
-            ma10 * mb01 + ma11 * mb11 + ma12 * mb21,
-            ma10 * mb02 + ma11 * mb12 + ma12 * mb22,
-
-            ma20 * mb00 + ma21 * mb10 + ma22 * mb20,
-            ma20 * mb01 + ma21 * mb11 + ma22 * mb21,
-            ma20 * mb02 + ma21 * mb12 + ma22 * mb22,
-        ]
-    }
+  multiply: function (a, b) {
+    const a00 = a[0 * 3 + 0];
+    const a01 = a[0 * 3 + 1];
+    const a02 = a[0 * 3 + 2];
+    const a10 = a[1 * 3 + 0];
+    const a11 = a[1 * 3 + 1];
+    const a12 = a[1 * 3 + 2];
+    const a20 = a[2 * 3 + 0];
+    const a21 = a[2 * 3 + 1];
+    const a22 = a[2 * 3 + 2];
+    const b00 = b[0 * 3 + 0];
+    const b01 = b[0 * 3 + 1];
+    const b02 = b[0 * 3 + 2];
+    const b10 = b[1 * 3 + 0];
+    const b11 = b[1 * 3 + 1];
+    const b12 = b[1 * 3 + 2];
+    const b20 = b[2 * 3 + 0];
+    const b21 = b[2 * 3 + 1];
+    const b22 = b[2 * 3 + 2];
+    return [
+      b00 * a00 + b01 * a10 + b02 * a20,
+      b00 * a01 + b01 * a11 + b02 * a21,
+      b00 * a02 + b01 * a12 + b02 * a22,
+      b10 * a00 + b11 * a10 + b12 * a20,
+      b10 * a01 + b11 * a11 + b12 * a21,
+      b10 * a02 + b11 * a12 + b12 * a22,
+      b20 * a00 + b21 * a10 + b22 * a20,
+      b20 * a01 + b21 * a11 + b22 * a21,
+      b20 * a02 + b21 * a12 + b22 * a22,
+    ];
+  },
 };
-
 /**
  * canvas 坐标转成 webgl坐标. 旋转时图形会变形？
  * @param {WebGLRenderingContext} gl
@@ -169,15 +139,14 @@ const m3 = {
  */
 
 function transformVec2(gl, arr) {
-    const halfWidth = gl.canvas.clientWidth / 2;
-    const halfHeight = gl.canvas.clientHeight / 2;
-    const res = [];
-    for (let i = 0; i < arr.length; i++) {
-        const x = (arr[i] - halfWidth) / halfWidth;
-        const y = -(arr[i + 1] - halfHeight) / halfHeight;
-        res.push(...[x, y]);
-        i++;
-    }
-    return res;
+  const halfWidth = gl.canvas.clientWidth / 2;
+  const halfHeight = gl.canvas.clientHeight / 2;
+  const res = [];
+  for (let i = 0; i < arr.length; i++) {
+    const x = (arr[i] - halfWidth) / halfWidth;
+    const y = -(arr[i + 1] - halfHeight) / halfHeight;
+    res.push(...[x, y]);
+    i++;
+  }
+  return res;
 }
-
--- packages/webgl/example/二维变换/二维变换.js
+++ packages/webgl/example/二维变换/二维变换.js
@@ -1,25 +1,28 @@
 const vertexShaderSource = `
-        attribute vec2 a_position;
-        uniform mat3 u_matrix;
-         void main(){
-           vec2 position = (u_matrix * vec3(a_position,1.0)).xy;
-           gl_Position = vec4(position,0.0,1.0);
-         }
-        `;
+      attribute vec2 a_position;
+      uniform vec2 u_resolution;
+      uniform mat3 u_matrix;
+      void main() { 
+        vec2 position = (u_matrix * vec3(a_position, 1)).xy;
+        vec2 zeroToOne = position / u_resolution;
+        vec2 zeroToTwo = zeroToOne * 2.0;
+        vec2 clipSpace = zeroToTwo - 1.0;
+        gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+      }`;
 const fragmentShaderSource = `
-        precision mediump float;
-        uniform vec4 u_color;
-        void main(){
-            gl_FragColor = u_color; 
-        }
-      `;
-const {program, gl} = initProgram(
-    "canvas",
-    vertexShaderSource,
-    fragmentShaderSource
+      precision mediump float;
+      uniform vec4 u_color;
+      void main(){
+          gl_FragColor = u_color; 
+      }`;
+const { program, gl } = initProgram(
+  "canvas",
+  vertexShaderSource,
+  fragmentShaderSource
 );
 
 const positionLocation = gl.getAttribLocation(program, "a_position");
+const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
 const colorLocation = gl.getUniformLocation(program, "u_color");
 const matrixLocation = gl.getUniformLocation(program, "u_matrix");
 
@@ -27,104 +30,84 @@ const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 setGeometry(gl);
 
-
 const translation = [100, 150];
-let angleInRadians = 90 * Math.PI / 180;
+let angleInRadians = (90 * Math.PI) / 180;
 const scale = [1, 1];
 const color = [Math.random(), Math.random(), Math.random(), 1];
 
-
 drawScene();
 
-webglLessonsUI.setupSlider("#x", {value: translation[0], slide: updatePosition(0), max: gl.canvas.width});
-webglLessonsUI.setupSlider("#y", {value: translation[1], slide: updatePosition(1), max: gl.canvas.height});
-webglLessonsUI.setupSlider("#angle", {slide: updateAngle, max: 360});
+webglLessonsUI.setupSlider("#x", {
+  value: translation[0],
+  slide: updatePosition(0),
+  max: gl.canvas.width,
+});
+webglLessonsUI.setupSlider("#y", {
+  value: translation[1],
+  slide: updatePosition(1),
+  max: gl.canvas.height,
+});
+webglLessonsUI.setupSlider("#angle", { slide: updateAngle, max: 360 });
 webglLessonsUI.setupSlider("#scaleX", {
-    value: scale[0],
-    slide: updateScale(0),
-    min: -5,
-    max: 5,
-    step: 0.01,
-    precision: 2
+  value: scale[0],
+  slide: updateScale(0),
+  min: -5,
+  max: 5,
+  step: 0.01,
+  precision: 2,
 });
 webglLessonsUI.setupSlider("#scaleY", {
-    value: scale[1],
-    slide: updateScale(1),
-    min: -5,
-    max: 5,
-    step: 0.01,
-    precision: 2
+  value: scale[1],
+  slide: updateScale(1),
+  min: -5,
+  max: 5,
+  step: 0.01,
+  precision: 2,
 });
 
 function updatePosition(index) {
-    return function (event, ui) {
-        translation[index] = ui.value;
-        drawScene();
-    };
+  return function (event, ui) {
+    translation[index] = ui.value;
+    drawScene();
+  };
 }
 
-
 function updateAngle(event, ui) {
-    const angleInDegrees = ui.value;
-    angleInRadians = angleInDegrees * Math.PI / 180;
-    drawScene();
+  var angleInDegrees = 360 - ui.value;
+  angleInRadians = (angleInDegrees * Math.PI) / 180;
+  drawScene();
 }
 
 function updateScale(index) {
-    return function (event, ui) {
-        scale[index] = ui.value;
-        drawScene();
-    };
+  return function (event, ui) {
+    scale[index] = ui.value;
+    drawScene();
+  };
 }
 
 function drawScene() {
-    gl.enableVertexAttribArray(positionLocation);
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-    gl.uniform4fv(colorLocation, color);
-    // const translationMatrix = m3.translation(translation[0], translation[1]);
-    const rotationMatrix = m3.rotation(angleInRadians);
-    // const scaleMatrix = m3.scale(scale[0], scale[1]);
-    // let matrix = m3.multiply(translationMatrix, rotationMatrix);
-    // matrix = m3.multiply(scaleMatrix, matrix);
-    clear(gl);
-    console.log(rotationMatrix);
-    gl.uniformMatrix3fv(matrixLocation, false, rotationMatrix);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-
+  gl.enableVertexAttribArray(positionLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+  gl.uniform4fv(colorLocation, color);
+  const translationMatrix = m3.translation(translation[0], translation[1]);
+  const rotationMatrix = m3.rotation(angleInRadians);
+  const scaleMatrix = m3.scaling(scale[0], scale[1]);
+  let matrix = m3.multiply(translationMatrix, rotationMatrix);
+  matrix = m3.multiply(matrix, scaleMatrix);
+  gl.uniformMatrix3fv(matrixLocation, false, matrix);
+  clear(gl);
+  gl.drawArrays(gl.TRIANGLES, 0, 18);
 }
 
 function setGeometry(gl) {
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(transformVec2(gl, [
-            gl.canvas.width / 2, gl.canvas.height / 2,
-            gl.canvas.width / 2 + 100, gl.canvas.height / 2,
-            gl.canvas.width / 2, gl.canvas.height / 2 + 100,
-
-            gl.canvas.width / 2, gl.canvas.height / 2 + 100,
-            gl.canvas.width / 2 + 100, gl.canvas.height / 2,
-            gl.canvas.width / 2 + 100, gl.canvas.height / 2 + 100,
-        ])),
-        gl.STATIC_DRAW
-    )
-    ;
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      0, 0, 30, 0, 0, 150, 0, 150, 30, 0, 30, 150, 30, 0, 100, 0, 30, 30, 30,
+      30, 100, 0, 100, 30, 30, 60, 67, 60, 30, 90, 30, 90, 67, 60, 67, 90,
+    ]),
+    gl.STATIC_DRAW
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
