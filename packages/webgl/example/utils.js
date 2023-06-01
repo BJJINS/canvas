0af22efad2541ad8e9d7cb59bda387697dab7580
BJJIN <bjjins@163.com>
2023-06-02 00:55:37 +0800

    3维度

--- packages/webgl/example/utils.js
+++ packages/webgl/example/utils.js
@@ -150,10 +150,7 @@ const m3 = {
     return m3.multiply(m, m3.scaling(sx, sy));
   },
   projection: function (width, height) {
-    return [
-      2 / width, 0, 0, 
-      0, -2 / height, 0, 
-      -1, 1,0];
+    return [2 / width, 0, 0, 0, -2 / height, 0, -1, 1, 0];
   },
 };
 /**
@@ -175,3 +172,59 @@ function transformVec2(gl, arr) {
   }
   return res;
 }
+
+const m4 = {
+  translation: function (tx, ty, tz) {
+    return [
+      1, 0, 0, 0, 
+      0, 1, 0, 0, 
+      0, 0, 1, 0, 
+      tx, ty, tz, 1
+    ];
+  },
+
+  xRotation: function (angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    return [
+      1, 0, 0, 0, 
+      0, c, s, 0, 
+      0, -s, c, 0, 
+      0, 0, 0, 1
+    ];
+  },
+
+  yRotation: function (angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    return [
+      c, 0, -s, 0, 
+      0, 1, 0, 0, 
+      s, 0, c, 0, 
+      0, 0, 0, 1
+    ];
+  },
+
+  zRotation: function (angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    return [
+      c, s, 0, 0, 
+      -s, c, 0, 0, 
+      0, 0, 1, 0, 
+      0, 0, 0, 1
+    ];
+  },
+
+  scaling: function (sx, sy, sz) {
+    return [
+      sx, 0, 0, 0, 
+      0, sy, 0, 0, 
+      0, 0, sz, 0, 
+      0, 0, 0, 1
+    ];
+  },
+};
